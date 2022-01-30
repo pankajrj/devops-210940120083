5c2b1913ef1222d6b4aa5cf83fd87b08e89203f3
pankajrj <pankajj241997@gmail.com>
2022-01-30 19:55:13 +0530

    java code added

---
+++ Avg.java
@@ -0,0 +1,9 @@
+class Avg{
+	public static void main(String[] args){
+		double a = 50;
+		double b = 60;
+		double c = 70;
+		double avg = (a + b + c)/2;
+		System.out.println("Average: "+ avg);
+	}
+}
